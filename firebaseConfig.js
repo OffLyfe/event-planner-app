97cda97697d8911e5c977c61e68e143c346d88d3
OffLyfe <t.klukay@gmail.com>
2026-05-17 15:24:25 +0200

    configure firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB0eJYSBBaKJQQtPI3rhWJU3hRVIDsCDOM",
+  authDomain: "event-planner-app-55e54.firebaseapp.com",
+  projectId: "event-planner-app-55e54",
+  storageBucket: "event-planner-app-55e54.firebasestorage.app",
+  messagingSenderId: "917124647066",
+  appId: "1:917124647066:web:500b0a4584d37782973723",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
