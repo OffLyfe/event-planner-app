8ea8f889e6ed04c5fb225a9e4c69d13e79bbe797
OffLyfe <t.klukay@gmail.com>
2026-05-17 17:46:47 +0200

    move authentication to login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,94 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { auth } from "../firebaseConfig";
 
-function LoginScreen() {
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+
+export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      alert("User registered!");
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const handleLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      alert("Logged in!");
+      navigation.navigate("Create Event");
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      alert("Logged out!");
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   return (
-    <View>
-      <Text>Login Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Event Planner App</Text>
+
+      <TextInput
+        placeholder="Email"
+        style={styles.input}
+        value={email}
+        onChangeText={setEmail}
+      />
+
+      <TextInput
+        placeholder="Password"
+        style={styles.input}
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+
+      <Button title="Register" onPress={handleRegister} />
+
+      <View style={{ marginTop: 10 }} />
+      <Button title="Login" onPress={handleLogin} />
+
+      <View style={{ marginTop: 10 }} />
+      <Button title="Logout" onPress={handleLogout} />
     </View>
   );
 }
 
-export default LoginScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+  },
+
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 30,
+    textAlign: "center",
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 12,
+    marginBottom: 15,
+    borderRadius: 8,
+  },
+});
