663c5c0fb3356f7e3741c69c8abb31881ac5f9e4
OffLyfe <t.klukay@gmail.com>
2026-05-17 19:43:45 +0200

    add event time picker

--- screens/CreateEventScreen.js
+++ screens/CreateEventScreen.js
@@ -2,12 +2,28 @@ import React, { useLayoutEffect, useState } from "react";
 import { View, Text, TextInput, Pressable, StyleSheet } from "react-native";
 import { collection, addDoc } from "firebase/firestore";
 import { signOut } from "firebase/auth";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+
 import { auth, db } from "../firebaseConfig";
 import { colors, spacing, radius } from "../theme";
 
 export default function CreateEventScreen({ navigation }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [location, setLocation] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [showPicker, setShowPicker] = useState(false);
+  const [time, setTime] = useState(new Date());
+  const [showTimePicker, setShowTimePicker] = useState(false);
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      navigation.replace("Login");
+    } catch (error) {
+      alert(error.message);
+    }
+  };
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -20,26 +36,27 @@ export default function CreateEventScreen({ navigation }) {
     });
   }, [navigation]);
 
-  const handleLogout = async () => {
-    try {
-      await signOut(auth);
-      navigation.replace("Login");
-    } catch (error) {
-      alert(error.message);
-    }
-  };
-
   const handleCreateEvent = async () => {
     try {
       await addDoc(collection(db, "events"), {
         title,
         description,
+        location,
+        date: date.toDateString(),
         createdAt: new Date(),
+        time: time.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+}),
       });
 
       alert("Event created!");
+
       setTitle("");
       setDescription("");
+      setLocation("");
+      setDate(new Date());
+
       navigation.navigate("Events");
     } catch (error) {
       alert(error.message);
@@ -65,6 +82,56 @@ export default function CreateEventScreen({ navigation }) {
         value={description}
         onChangeText={setDescription}
       />
+
+      <TextInput
+        placeholder="Location"
+        placeholderTextColor={colors.muted}
+        style={styles.input}
+        value={location}
+        onChangeText={setLocation}
+      />
+
+      <Pressable style={styles.dateButton} onPress={() => setShowPicker(true)}>
+        <Text style={styles.dateButtonText}>{date.toDateString()}</Text>
+      </Pressable>
+      
+      <Pressable
+  style={styles.dateButton}
+  onPress={() => setShowTimePicker(true)}
+>
+  <Text style={styles.dateButtonText}>
+    {time.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+    })}
+  </Text>
+</Pressable>
+
+<DateTimePickerModal
+  isVisible={showTimePicker}
+  mode="time"
+  date={time}
+  isDarkModeEnabled={false}
+  themeVariant="light"
+  onConfirm={(selectedTime) => {
+    setTime(selectedTime);
+    setShowTimePicker(false);
+  }}
+  onCancel={() => setShowTimePicker(false)}
+/>
+
+<DateTimePickerModal
+  isVisible={showPicker}
+  mode="date"
+  date={date}
+  isDarkModeEnabled={false}
+  themeVariant="light"
+  onConfirm={(selectedDate) => {
+    setDate(selectedDate);
+    setShowPicker(false);
+  }}
+  onCancel={() => setShowPicker(false)}
+/>
 
       <Pressable style={styles.primaryButton} onPress={handleCreateEvent}>
         <Text style={styles.primaryButtonText}>Create Event</Text>
@@ -101,6 +168,17 @@ const styles = StyleSheet.create({
     borderRadius: radius.md,
     padding: 14,
     marginBottom: spacing.md,
+    color: colors.text,
+  },
+  dateButton: {
+    backgroundColor: "#F9FAFB",
+    borderWidth: 1,
+    borderColor: colors.border,
+    borderRadius: radius.md,
+    padding: 14,
+    marginBottom: spacing.md,
+  },
+  dateButtonText: {
     color: colors.text,
   },
   primaryButton: {
